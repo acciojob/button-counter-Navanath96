cffab03a57ae5eaf113f5f45431c365309361f5f
navanath <navanath96@gmail.com>
2023-07-24 14:33:49 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [click,setclick]=useState("0");
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>
+          Button clicked {click} times
+        </p>
+        <button type="submit" onClick={()=>{
+          setclick(click+1);
+        }}>Click me</button>
     </div>
   )
 }
